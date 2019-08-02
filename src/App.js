1c09bf92062b75084164a41014fb9f77c3db6385
Wang <ywang24a7d7@statestreet.com>
2019-08-01 23:50:42 -0400

    Updated black jack and overcome the setstate callback hell issue with promise.

---
+++ src/App.js
@@ -0,0 +1,248 @@
+import React from 'react';
+import axios from 'axios';
+
+import './App.css';
+
+const baseCls = 'home';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      deck_id: '',
+      customer: {},
+      dealer: {},
+      result: '',
+    };
+
+    this.hitCard = this.hitCard.bind(this);
+    this.getCard = this.getCard.bind(this);
+  }
+
+  componentDidMount() {
+    axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
+      .then(res => {
+        this.setState({
+          deck_id: res.data.deck_id,
+        }, () => {
+          this.getCard(false)
+            .then((res) => {
+              this.getCard(true)
+                .then(() => {
+                  this.getCard(false)
+                    .then(() => {
+                      this.getCard(true);
+                    });
+                });
+            });
+        });
+      });
+  }
+
+  getCard(val) {
+    const original = val ? this.state.customer : this.state.dealer;
+    const promise = new Promise((success) => {
+      axios.get(`https://deckofcardsapi.com/api/deck/${this.state.deck_id}/draw/?count=1`)
+        .then(res => {
+          console.log('api response: ', res)
+          const temp = { ...original };
+          console.log('original: ', temp);
+          temp[Object.keys(temp).length] = res.data.cards[0];
+          console.log('after: ', temp);
+
+          if (val) {
+            this.setState({
+              customer: temp,
+            }, () => {
+              success();
+            });
+          } else {
+            this.setState({
+              dealer: temp,
+            }, () => {
+              success();
+            });
+          }
+        })
+    });
+    return promise;
+  }
+
+  drwaUnique(n) {
+    let i = 0;
+    const resArr = [];
+    axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
+      .then(res => {
+        function drawCard(val) {
+          for (var i = resArr.length; i < n; i++) {
+            axios.get(`https://deckofcardsapi.com/api/deck/${res.deck_id}/draw/?count=1`)
+              .then(currRes => {
+                let isExisting = false;
+                resArr.forEach(e => {
+                  if (currRes.value === e.value) {
+                    isExisting = true;
+                  }
+                  if (isExisting) {
+                    drawCard();
+                  } else {
+                    resArr.push(currRes);
+                  }
+                })
+              })
+          }
+        }
+        drawCard();
+        return resArr;
+      })
+  }
+
+  hitCard() {
+    axios.get(`https://deckofcardsapi.com/api/deck/${this.state.deck_id}/draw/?count=1`)
+      .then(res => {
+        const temp = { ...this.state.customer };
+        temp[Object.keys(temp).length] = res.data.cards[0];
+        this.setState({
+          customer: temp,
+        }, () => {
+          // console.log(this.state.customer);
+          // let dealerSum = 0;
+          // Object.keys(this.state.dealer).forEach(key => {
+          //   if (key) {
+          //     dealerSum += this.state.dealer[key];
+          //   }
+          // });
+          // const limit = dealerSum + 10;
+          // dealerSum = dealerSum + this.state.dealer[0];
+          // if (dealerSum < limit) {
+          //   axios.get(`https://deckofcardsapi.com/api/deck/${this.state.deck_id}/draw/?count=1`)
+          //     .then(dealRes => {
+          //       const tempDealer = { ...this.state.dealer };
+          //       tempDealer[Object.keys(tempDealer).length] = tempDealer.data;
+          //       this.setState({
+          //         dealer: tempDealer,
+          //       });
+          //     })
+          // }
+        })
+      })
+  }
+
+  render() {
+    const {
+      customer,
+      dealer,
+    } = this.state;
+    let customerSum = 0;
+    Object.values(this.state.customer).forEach(card => {
+      if (card.value === 'JACK' || card.value === 'QUEEN' || card.value === 'KING') {
+        customerSum += 10;
+      } else if (card.value === 'ACE') {
+        const tempOne = customerSum + 1;
+        const tempEleven = customerSum + 11;
+        const tempOneDiffer = 21 - tempOne;
+        const tempElevenDiffer = 21 - tempEleven;
+        if (tempOneDiffer < tempElevenDiffer) {
+          customerSum = tempOne;
+        } else {
+          customerSum = tempEleven;
+        }
+      } else {
+        customerSum += Number(card.value);
+      }
+    });
+    return (
+      <div className={`${baseCls}`}>
+        <div className={`${baseCls}__header`}>Hello, customer, black jack here!</div>
+        <div className={`${baseCls}__dealer`}>
+          <div className={`${baseCls}__table-title`}>Dealer Table</div>
+          <div className={`${baseCls}__render-cards`}>
+            {
+              Object.values(dealer).map((card, i) => {
+                if (i) {
+                  return (
+                    <div
+                      key={`${card.key}-${card.suit}`}
+                      className={`${baseCls}__card`}
+                    >
+                      <img
+                        className={`${baseCls}_card-img`}
+                        src={`${card.image}`}
+                      />
+                    </div>
+                  );
+                } else {
+                  return (
+                    <div
+                      key={`${card.code}-${card.suit}`}
+                      className={`${baseCls}__card`}
+                    >
+                      <img
+                        className={`${baseCls}__card-img-hide-dealer`}
+                        src={`${card.image}`}
+                      />
+                      {
+                        customerSum > 21
+                          ? (
+                            <div className={`${baseCls}__result`}>You Lose, please refresh to play again.</div>
+                          )
+                          : null
+                      }
+                    </div>
+                  )
+                }
+              })
+            }
+          </div>
+        </div>
+        <div className={`${baseCls}__customer`}>
+          <div className={`${baseCls}__table-title`}>Customer Table</div>
+          <div className={`${baseCls}__render-cards`}>
+            {
+              Object.values(customer).map((card, i) => {
+                if (i) {
+                  return (
+                    <div
+                      key={`${card.code}-${card.suit}`}
+                      className={`${baseCls}__card`}
+                    >
+                      <img
+                        className={`${baseCls}_card-img`}
+                        src={`${card.image}`}
+                      />
+                    </div>
+                  );
+                } else {
+                  return (
+                    <div
+                      key={`${card.key}-${card.suit}`}
+                      className={`${baseCls}__card`}
+                    >
+                      <img
+                        className={`${baseCls}__card-img-hide`}
+                        src={`${card.image}`}
+                      />
+                    </div>
+                  )
+                }
+              })
+            }
+          </div>
+          <div className={`${baseCls}__customer-options`}>
+            <div
+              className={`${baseCls}__hit`}
+              onClick={() => {
+                this.hitCard();
+              }}
+            >
+              Hit
+            </div>
+            <div className={`${baseCls}__stand`}>Stand</div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
